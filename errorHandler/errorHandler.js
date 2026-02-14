d5f03bf47c7aba3cf91491d26e2009cd822dbf02
vishnu <vishnuprakash4396@gmail.com>
2026-02-14 21:00:07 +0530

    Update server error message

--- errorHandler/errorHandler.js
+++ errorHandler/errorHandler.js
@@ -3,7 +3,7 @@ const errorHandler = (error, req, res, next) => {
     console.log(error)
     res.status(500).json({
         success: false,
-        message: 'Internal hi server error'
+        message: 'Internal server error'
     })
    
 }
